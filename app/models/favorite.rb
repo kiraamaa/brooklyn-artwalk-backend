41904e9c92321a2ac24fde099bd219b4024d3024
kiraamaa <kira.a.maclean@gmail.com>
2017-01-20 09:13:02 -0500

    Add validation to favorite model
    Now same artwalk cannot be added to favorites multiple times.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,6 @@
 class Favorite < ActiveRecord::Base
   belongs_to :artwalk, inverse_of: :favorites
   belongs_to :user, inverse_of: :favorites
+  validates :title, uniqueness: true
   # has_many :walks
 end
--- scripts/create-favorite.sh
+++ scripts/create-favorite.sh
@@ -3,7 +3,7 @@ curl --include --request POST http://localhost:4741/favorites \
   --data '{
     "favorite": {
       "title": "new fave",
-      "artwalk_id": "13",
+      "artwalk_id": "67",
       "user_id": "1"
     }
   }'
