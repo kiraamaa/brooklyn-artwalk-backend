5d47e80657ef8cf975fb2b48ceed0035440f21a3
kiraamaa <kira.a.maclean@gmail.com>
2017-01-20 11:13:19 -0500

    Add validation to favorite model
    Validates for presence of both artwalk and user id and no longer
    validates uniqueness of title. Rails console shows that multiple
    artwalks can be made for a given user and different users can have
    artwalks with the same name.

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,8 @@
 class Favorite < ActiveRecord::Base
   belongs_to :artwalk, inverse_of: :favorites
   belongs_to :user, inverse_of: :favorites
-  validates :title, uniqueness: true
+  validates :artwalk_id, presence: true
+  validates :user_id, presence: true
+  validates :user_id, uniqueness: { scope: :artwalk_id }
   # has_many :walks
 end
